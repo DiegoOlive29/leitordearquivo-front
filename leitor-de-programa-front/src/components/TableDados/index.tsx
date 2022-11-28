c9b94455fd220bb42c211e746bf58faaa8d3f673
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2022-11-28 12:54:02 -0300

    Create style table and list

--- leitor-de-programa-front/src/components/TableDados/index.tsx
+++ leitor-de-programa-front/src/components/TableDados/index.tsx
@@ -1,5 +1,5 @@
 import { Responsepost } from "../../service/viewsApi";
-import { TableList } from "./styles";
+import { TableItem, TableList } from "./styles";
 
 export interface ObjResponse{
     
@@ -8,11 +8,18 @@ export interface ObjResponse{
 const TableDado = ({dados}:ObjResponse) => {
   return (
         <TableList>
+           <TableItem>
+          <p>Tipo op</p>
+          <span>Nome da Loja</span>
+          <span>Valor:</span>
+          </TableItem>
         {dados === undefined  ? ("Sem registros") :(dados?.map((dado:Responsepost)=>
          { return(
-          
-          <p> {dado.id}</p>
-        
+          <TableItem>
+          <p> {dado.typeOp}</p>
+          <div>{dado.store_name}</div>
+          <span>R${dado.value},00</span>
+          </TableItem>
 
 
 
